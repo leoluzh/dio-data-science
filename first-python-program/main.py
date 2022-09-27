a24c05b9a2114ce42a432c490748f6d11502a459
leonardo luz fernandes <leonardo.l.fernandes@gmail.com>
2022-09-26 21:22:03 -0300

    Programa simples em Python

---
+++ first-python-program/main.py
@@ -0,0 +1,16 @@
+# This is a Simple Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name} welcome to python environment')  # Press ⌘F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('Leonardo')
+
+
